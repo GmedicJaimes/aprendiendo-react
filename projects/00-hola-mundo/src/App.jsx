3da6b98af64223ee672bcfd164f4e0c95394e358
Gulliano Jaimes <88950788+GmedicJaimes@users.noreply.github.com>
2023-09-27 17:12:28 -0500

    Componentes en react

---
+++ projects/00-hola-mundo/src/App.jsx
@@ -0,0 +1,25 @@
+import "./App.css";
+
+const App = () => {
+  return (
+    <article className="tw-followCard">
+      <header className="tw-followCard-header">
+        <img
+          className="tw-followCard-avatar"
+          src="https://unavatar.io/midudev"
+          alt="avatar de midu"
+        />
+        <div className="tw-followCard-info">
+          <strong>Miguel Ángel Durán</strong>
+          <span className="tw-followCard-infoUserName">@midudev</span>
+        </div>
+      </header>
+
+      <aside>
+        <button className="tw-followCard-button">Seguir</button>
+      </aside>
+    </article>
+  );
+};
+
+export default App;
